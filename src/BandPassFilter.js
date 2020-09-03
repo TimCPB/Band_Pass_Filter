4293378e00491b4d4e9eb383d8995c2a893d40c1
Tim <TimCPB@github.com>
2020-09-03 16:52:56 +0100

    green test 'returns a filtered array'

--- src/BandPassFilter.js
+++ src/BandPassFilter.js
@@ -5,20 +5,17 @@ class BandPassFilter {
   }
 
   filter(array){
-      // array.forEach(function(num){
-      //   if(num > this.upperLimit){
-      //     num = this.upperLimit
-      //   }
-      //   else return num
-      // })
-      // return this.upperLimit
-      if(array[0] > this.upperLimit){
-        return [this.upperLimit]
-      }
-      else if(array[0] < this.lowerLimit){
-        return [this.lowerLimit]
-      }
-      else
-        return array
+      var newArray = []
+      array.forEach(function(number) {
+        if(number > this.upperLimit){
+          newArray.push(this.upperLimit)
+        }
+        else if(number < this.lowerLimit){
+          newArray.push(this.lowerLimit)
+        }
+        else
+          newArray.push(number)
+        }, this)
+        return newArray
       }
     }
