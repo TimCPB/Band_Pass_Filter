4bf3dd8af636bc91ec55fab4d7f0ab72286eda34
Tim <TimCPB@github.com>
2020-09-03 14:24:44 +0100

    red rest: 'A new filter has a lower limit of 40 and an upper limit of 1000'

---
+++ spec/BandPassFilterSpec.js
@@ -0,0 +1,12 @@
+describe("BandPassFilter", function(){
+  var BandPassFilter
+
+  beforeEach(function() {
+    bandPassFilter = new BandPassFilter();
+  })
+
+  it("A new filter has a lower limit of 40 and an upper limit of 1000", function() {
+    expect(bandPassFilter.lowerLimit).toEqual(40)
+    expect(bandPassFilter.upperLimit).toEqual(1000)
+  })
+})
